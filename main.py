09761782d2b71c656c233eda28954f59a055e7b3
Anton <abramov1991a@gmail.com>
2024-05-05 20:05:36 +0500

    add check func

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import os
 import requests
+from requests.exceptions import HTTPError
 
 
 url = 'https://tululu.org/txt.php'
 
-response = requests.get(url)
-response.raise_for_status()
-
 if not os.path.exists('books'):
   os.makedirs('books')
 
+
+def check_for_redirect(response):
+  if response.url == 'https://tululu.org/':
+    raise HTTPError(f'error')
+
+
 for id in range(1, 11):
   payload = {'id': id}
-  response = requests.get(url, params=payload)
-  response.raise_for_status()
 
   book = f'books/id{id}.txt'
+  response = requests.get(url, params=payload)
+  response.raise_for_status()
+  
+  try:
+    check_for_redirect(response)
+  except HTTPError as http_err:
+    print(http_err)
+    continue
 
   with open(book, 'wb') as file:
-    file.write(response.content)
+      file.write(response.content)
