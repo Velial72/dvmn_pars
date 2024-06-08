854f85dee391339892ec96bb1296dde8722adf11
Anton <abramov1991a@gmail.com>
2024-06-08 18:19:49 +0500

    del some trash

--- help_functions.py
+++ help_functions.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 from requests.exceptions import HTTPError
 from pathvalidate import sanitize_filename, sanitize_filepath
-from urllib.parse import urljoin, urlparse
+from urllib.parse import urljoin
 
 
 def check_for_redirect(response):
