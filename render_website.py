e42a7d6dfc49b6466a99fef7d077dcabcc012b04
Anton <abramov1991a@gmail.com>
2024-06-29 19:33:51 +0500

    Split books list by pages.

--- render_website.py
+++ render_website.py
@@ -1,9 +1,10 @@
+import os
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
 from pathlib import Path
-
+import math
 
 json_path = Path('books/Book_info.json')
 with open(json_path, 'r', encoding='utf8') as file:
@@ -27,10 +28,20 @@ books_info = [
     for book in books_data
 ]
 
-rendered_page = template.render(books=books_info)
+books_per_page = 10
+total_pages = math.ceil(len(books_info) / books_per_page)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+os.makedirs('pages', exist_ok=True)
+
+for page_num in range(1, total_pages + 1):
+    start_index = (page_num - 1) * books_per_page
+    end_index = start_index + books_per_page
+    current_page_books = books_info[start_index:end_index]
+
+    rendered_page = template.render(books=current_page_books, page_num=page_num, total_pages=total_pages)
+
+    with open(f'pages/index{page_num}.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
